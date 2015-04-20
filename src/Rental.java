56aeab1129e2336716d88ba6c9874fd3076014dd
ytalia1994 <ytalia94@yahoo.de>
2015-04-20 15:21:25 +0200

    Replace Conditional with Polymorphism

--- src/Rental.java
+++ src/Rental.java
@@ -1,45 +1,24 @@
 class Rental {
-	private Movie movie;
-	private int daysRented;
+    private Movie movie;
+    private int daysRented;
+    public Rental(Movie newmovie, int newdaysRented) {
+        movie = newmovie;
+        daysRented = newdaysRented;
+    }
+    public int getDaysRented() {
+        return daysRented;
+    }
+    public Movie getMovie() {
+        return movie;
+    }
 
-	public Rental(Movie newmovie, int newdaysRented) {
-		movie = newmovie;
-		daysRented = newdaysRented;
-	}
+    public double getCharge() {
+        return getMovie().getCharge(getDaysRented());
+    }
 
-	public int getDaysRented() {
-		return daysRented;
-	}
+    public int getFrequentRenterPoints() {
+        return getMovie().getFrequentRenterPoints(getDaysRented());
+    }
 
-	public Movie getMovie() {
-		return movie;
-	}
 
-	double getCharge() {
-		double result = 0;
-		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return result;
-	}
-
-	int getFrequentRenterPoints() {
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& getDaysRented() > 1)
-			return 2;
-		else
-			return 1;
-	}
 }
